5cc02178bb1f08c7abd73ccb811cd2a31fc1b327
Luigivalente <91776312+Luigivalente@users.noreply.github.com>
2021-12-01 09:52:49 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,32 @@
 #include <iostream>
+using namespace std;
 
-int main(){
-  return 0;
+bool primo(int n, int i) {
+    if(n==2){
+        return true;
+    }
+    if(n==1){
+        return false;
+    }
+    if (n % i == 0) {
+        return false;
+    } else {
+        i++;
+        if (i == n) {
+            return true;
+        } else {
+            primo(n, i);
+        }
+    }
+}
+
+int main() {
+    int n, i=2;
+    cin >> n;
+    if(primo(n,i)){
+        cout << "numero primo"<<endl;
+    } else {
+        cout << "numero non primo"<<endl;
+    }
+    return 0;
 }
