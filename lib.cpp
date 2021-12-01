4754c44d7bec9a072764aa8fd3176a7d947b1701
Luigivalente <91776312+Luigivalente@users.noreply.github.com>
2021-12-01 12:12:51 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,18 +1,25 @@
 #include "lib.h"
-bool primo(int n, int i) {
-    if(n==2){
+bool primo(int n, int i) 
+{
+    if(n==2)
+    {
         return true;
     }
-    if(n==1){
+    if(n==1)
+    {
         return false;
     }
-    if (n % i == 0) {
+    if (n % i == 0)
+    {
         return false;
-    } else {
+    } else
+    {
         i++;
-        if (i == n) {
+        if (i == n) 
+        {
             return true;
-        } else {
+        } else 
+        {
             primo(n, i);
         }
     }
