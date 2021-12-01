1cf933f6affd2819bbf3ee245da64caec3a80a84
Luigivalente <91776312+Luigivalente@users.noreply.github.com>
2021-12-01 10:05:26 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,18 @@
-
+bool primo(int n, int i) {
+    if(n==2){
+        return true;
+    }
+    if(n==1){
+        return false;
+    }
+    if (n % i == 0) {
+        return false;
+    } else {
+        i++;
+        if (i == n) {
+            return true;
+        } else {
+            primo(n, i);
+        }
+    }
+}
