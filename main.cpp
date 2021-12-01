7d7e7e9ebc915d17d4a04e09086c06670a06b725
Luigivalente <91776312+Luigivalente@users.noreply.github.com>
2021-12-01 12:05:33 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,6 @@
 #include <iostream>
+#include "lib.h"
 using namespace std;
-
-bool primo(int n, int i) {
-    if(n==2){
-        return true;
-    }
-    if(n==1){
-        return false;
-    }
-    if (n % i == 0) {
-        return false;
-    } else {
-        i++;
-        if (i == n) {
-            return true;
-        } else {
-            primo(n, i);
-        }
-    }
-}
 
 int main() {
     int n, i=2;
